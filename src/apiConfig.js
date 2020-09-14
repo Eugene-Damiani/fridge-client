9c0b1897e8ce1833751bd803ddf7460032a69ccb
Eugene-Damiani <damianieugene@gmail.com>
2020-09-14 05:59:17 -0700

    production

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://stark-falls-98169.herokuapp.com',
+  production: 'https://tranquil-crag-61634.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
